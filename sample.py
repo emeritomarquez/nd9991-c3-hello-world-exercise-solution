33b9b653fb781f97ff1b8ac99190ffc0d03b1912
EC2 Default User <ec2-user@ip-172-31-29-68.us-west-2.compute.internal>
2022-04-21 21:06:10 +0000

    Add python script to select new EC2 instances in the inventory.txt file

---
+++ sample.py
@@ -0,0 +1,30 @@
+f = open('inventory.txt', 'r')
+lines = f.readlines()
+f.close()
+rows = []
+for line in lines:
+    if line == "[hosts]\n":
+        rows.append(line)
+    elif line == "172.31.18.180\n":
+        pass
+    elif line == "172.31.4.250\n":
+        pass
+    elif line == "172.31.2.246\n":
+        pass
+    elif line == "172.31.5.120\n":
+        pass
+    elif line == "172.31.13.218\n":
+        pass
+    elif line == "172.31.9.5\n":
+        pass
+    elif line == "172.31.29.68\n":
+        pass
+    else:
+        rows.append(line)
+print(rows)
+g = open('inventory1.txt', 'w+')
+for element in rows:
+    g.write(element)
+print(g)
+g.close()
+
