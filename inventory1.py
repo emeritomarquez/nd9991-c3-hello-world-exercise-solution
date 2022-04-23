d27cf9820891ab1900bdb7b6856ac4b4b723c01f
EC2 Default User <ec2-user@ip-172-31-29-68.us-west-2.compute.internal>
2022-04-23 20:37:12 +0000

    rename inventory.py to inventory1.py

---
+++ inventory1.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import sys
+
+f = open(sys.argv[1], 'r')
+lines = f.readlines()
+f.close()
+rows = []
+for line in lines:
+    if line == "[hosts]\n":
+        rows.append(line)
+    elif line == "172.31.18.180\n":
+        pass
+    elif line == "172.31.4.250\n":
+        pass
+    elif line == "172.31.2.246\n":
+        pass
+    elif line == "172.31.5.120\n":
+        pass
+    elif line == "172.31.13.218\n":
+        pass
+    elif line == "172.31.9.5\n":
+        pass
+    elif line == "172.31.29.68\n":
+        pass
+    else:
+        rows.append(line)
+print(rows)
+g = open(sys.argv[2], 'w+')
+for element in rows:
+    g.write(element)
+print(g)
+g.close()
+
