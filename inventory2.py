bdf25fc136633bb32c425ed404705a4e61c8969e
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2022-06-20 21:18:26 +0000

    Current first run entering current ec2 public ip addresses

--- inventory2.py
+++ inventory2.py
@@ -9,11 +9,13 @@ rows = []
 for line in lines:
     if line == "[hosts]\n":
         rows.append(line)
-    elif line == "54.189.130.180\n":
+    elif line == "35.87.96.184\n":
         pass
-    elif line == "34.219.157.212\n":
+    elif line == "54.69.86.178\n":
         pass
-    elif line == "35.160.54.251\n":
+    elif line == "54.186.184.223\n":
+        pass
+    elif line == "34.221.45.78\n":
         pass
     else:
         rows.append(line)
