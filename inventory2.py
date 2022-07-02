617395424831a0b1d181607fbfb365576e3932ba
Ubuntu <ubuntu@ip-172-31-1-88.us-west-2.compute.internal>
2022-07-02 12:46:21 +0000

    Edit of Public IP addresses of the ec2 instances

--- inventory2.py
+++ inventory2.py
@@ -9,33 +9,44 @@ rows = []
 for line in lines:
     if line == "[hosts]\n":
         rows.append(line)
+#Windows AWS Work Machine
     elif line == "172.31.18.180\n":
         pass
+#Ubuntu    
     elif line == "172.31.4.250\n":
         pass
+#app01 prometheus
     elif line == "172.31.2.246\n":
         pass
+#lb01
     elif line == "172.31.5.120\n":
         pass
+#app02 prometheus node exporter-pip-ansible
     elif line == "172.31.13.218\n":
         pass
+#nodejs-server-port-3000
     elif line == "172.31.9.5\n":
         pass
+#Kali Linux
     elif line == "172.31.32.205\n":
         pass
+#amazon-linux2
     elif line == "172.31.29.68\n":
         pass
+#linux-ec2-ansible-test
     elif line == "172.31.25.22\n":
         pass
+#ansible-test
     elif line == "172.31.28.42\n":
         pass
+#Ubuntu 22.04
     elif line == "172.31.1.88\n":
         pass
+#RedHat-Ansible
     elif line == "172.31.4.91\n":
         pass
+#Ubuntu18.04.6-ansible
     elif line == "172.31.13.83\n":
-        pass
-    elif line == "172.31.20.231\n":
         pass
     else:
         rows.append(line)
